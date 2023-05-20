6dd9f22ed5f3e5e958e93cb1f76c8e3be24c07fe
Angelica Beltran <angelica.beltran11@gmail.com>
2023-05-20 16:45:01 -0500

    finally fixed errors of require not being defined, etc

--- server/routes/tasksRouter.js
+++ server/routes/tasksRouter.js
@@ -1,29 +1,43 @@
-// app.use('/tasksRouter', tRouter);
 
 
 const express = require('express');
-const expressRouter = express.Router();
+const tasksRouter = express.Router();
 
 const pool = require('../modules/pool');
 
+
 tasksRouter.post('/', (req, res) => {
+    console.log('reached the tRouter for post');
+    console.log(req.body);
+
     const meh = req.body;
-    // console.log('reached the tRouter for post', meh);
     let queryText = `CREATE TABLE "toDoList"
                         ("id", "task")
                         VALUES ($1, $2);
         `;
-    const values = [
-        meh.id,
-        meh.task
-    ];
-    console.log(values);
-}).then((result) => {
-    res.sendStatus(200);
-}).catch((error) => {
-    console.log('error at tRouter post');
-    res.sendStatus(500);
-})
+        pool.query(queryText, [meh.id, meh.task]
+            
+            ).then((result) => {    
+                res.sendStatus(200);
+            }).catch((error) => {
+                console.log('error at tRouter post');
+                res.sendStatus(500);
+            })
+        });
+
+    // const values = [
+    //     meh.id,
+    //     meh.task
+    // ];
+
+//     // console.log(values);
+// }).then(result => {
+//     res.sendStatus(200);
+// }).catch(error => {
+//     console.log('error at tRouter post', error);
+//     res.sendStatus(500);
+// });
+
 
 tasksRouter.get('/', (req,res) =>{
     let queryText = `SELECT * FROM "toDoList"`
